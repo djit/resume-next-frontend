95d4e2fdd20669afaf7429a8f9d3a85a93efbb23
Djilali Tabbouche <dtabbouche@gmail.com>
2022-03-14 21:00:07 +0100

    commented getStaticProps function on /projects page

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -17,18 +17,18 @@ export default function Projects() {
 }
 
 
-export async function getStaticProps(context) {
-  const result = await fetch('http://localhost:1337/api/posts?sort[0]=createdAt:desc', {
-      headers: {
-        Authorization: 'Bearer 91bc8c5dd2059bfe409b7555f4d5b9d5cbcf3070c6c97eaf2e894f9fd6116a2c83d394553630e082e0b67a15a2fef81283095c78166cc974a56bc18e94f14235158e40db5bf40470ecc52297143f45d02601ecf6fa31ba4d00ebb9ba04a2338b38941d47b677458badb35622f88a0a4249085f18dc07cb38ca02a749b777f1af'
-      }
-    }
-  );
-  const data = await result.json()
-  //console.log(data.data)
-  return {
-    props: {
-      posts: { data: data.data }
-    }
-  }
-}
+// export async function getStaticProps(context) {
+//   const result = await fetch('http://localhost:1337/api/posts?sort[0]=createdAt:desc', {
+//       headers: {
+//         Authorization: 'Bearer 91bc8c5dd2059bfe409b7555f4d5b9d5cbcf3070c6c97eaf2e894f9fd6116a2c83d394553630e082e0b67a15a2fef81283095c78166cc974a56bc18e94f14235158e40db5bf40470ecc52297143f45d02601ecf6fa31ba4d00ebb9ba04a2338b38941d47b677458badb35622f88a0a4249085f18dc07cb38ca02a749b777f1af'
+//       }
+//     }
+//   );
+//   const data = await result.json()
+//   //console.log(data.data)
+//   return {
+//     props: {
+//       posts: { data: data.data }
+//     }
+//   }
+// }
