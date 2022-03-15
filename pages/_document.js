c1176ac9af1eba4ef35c703e255deaf8227f47c0
Djilali Tabbouche <dtabbouche@gmail.com>
2022-03-15 22:14:21 +0100

    google tag comment fix

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
         <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'></link>
         <link rel="shortcut icon" type="image/x-icon" href="/assets/img/favicon.ico"/>
 
-        <!-- Global site tag (gtag.js) - Google Analytics -->
+        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-QHJ79SGB40"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
