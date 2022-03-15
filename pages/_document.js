d6795dd6b4de84e712eaedb29eae6fe507a9cd10
Djilali Tabbouche <dtabbouche@gmail.com>
2022-03-15 22:21:20 +0100

    google tag fix

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -8,7 +9,7 @@ export default function Document() {
         <link rel="shortcut icon" type="image/x-icon" href="/assets/img/favicon.ico"/>
 
         {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-QHJ79SGB40" strategy="afterInteractive"></script>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-QHJ79SGB40" strategy="afterInteractive"></Script>
         <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
