447efb54f3b104eb91e2b0db6815b981aad64bc0
Djilali Tabbouche <dtabbouche@gmail.com>
2022-03-15 22:25:52 +0100

    moved google analytics to from _document.js to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
+import Script from 'next/script'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-QHJ79SGB40" strategy="afterInteractive"></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){window.dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-QHJ79SGB40');
+      `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
+
 
 export default function Document() {
   return (
@@ -7,18 +7,6 @@ export default function Document() {
       <Head>
         <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'></link>
         <link rel="shortcut icon" type="image/x-icon" href="/assets/img/favicon.ico"/>
-
-        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-QHJ79SGB40" strategy="afterInteractive"></Script>
-        <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-QHJ79SGB40');
-        `}
-        </Script>
       </Head>
       <body className="bg-gray-100 antialiased">
         <Main />
