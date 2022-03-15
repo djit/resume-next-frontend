ce65f1d538bd4673735c7fafb49d72cbf208f96b
Djilali Tabbouche <dtabbouche@gmail.com>
2022-03-15 22:13:34 +0100

    added Google Tag for analytics

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,16 @@ export default function Document() {
       <Head>
         <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'></link>
         <link rel="shortcut icon" type="image/x-icon" href="/assets/img/favicon.ico"/>
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-QHJ79SGB40"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-QHJ79SGB40');
+        </script>
       </Head>
       <body className="bg-gray-100 antialiased">
         <Main />
