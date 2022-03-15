e1b8daddf981968c598310803fb5d9921f7eeefb
Djilali Tabbouche <dtabbouche@gmail.com>
2022-03-15 21:02:33 +0100

    added favicon link

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'></link>
+        <link rel="shortcut icon" type="image/x-icon" href="/assets/img/favicon.ico"/>
       </Head>
       <body className="bg-gray-100 antialiased">
         <Main />
