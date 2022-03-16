ff62ba7e1d1c1ed6f10395960c07e64481d2c97a
Djilali Tabbouche <dtabbouche@gmail.com>
2022-03-16 23:55:04 +0100

    added production env test for displaying google analytics code

--- pages/_app.js
+++ pages/_app.js
@@ -3,19 +3,25 @@ import Script from 'next/script'
 
 
 function MyApp({ Component, pageProps }) {
+  const env = process.env.NODE_ENV
+
   return (
     <>
       {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-QHJ79SGB40" strategy="afterInteractive"></Script>
-      <Script id="google-analytics" strategy="afterInteractive">
-      {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){window.dataLayer.push(arguments);}
-        gtag('js', new Date());
+      { env === 'production' ?
+      <>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-QHJ79SGB40" strategy="afterInteractive"></Script>
+        <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-        gtag('config', 'G-QHJ79SGB40');
-      `}
-      </Script>
+          gtag('config', 'G-QHJ79SGB40');
+        `}
+        </Script>
+      </>
+      : "" }
 
       <Component {...pageProps} />
     </>
