7870d65e49f9883382f3e883589eb897cd438a64
Djilali Tabbouche <dtabbouche@gmail.com>
2022-03-21 18:40:57 +0100

    revert revalidate on blog index page

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -33,8 +33,7 @@ export async function getServerSideProps(context) {
     const posts = data.data
     //console.log(posts)
     return {
-      props: { posts },
-      revalidate: 60 //revalidate every minute
+      props: { posts }
     }
   } else {
     // Handle errors
