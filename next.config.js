bb707b212eb3e98a2145b8a03ad514ff05bb416f
Djilali Tabbouche <dtabbouche@gmail.com>
2022-12-28 01:22:37 +0100

    update images domains list

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost', 'github.com', 'resume-strapi-backend.onrender.com', 'api.djilali.tabbouche.com']
+    domains: ['localhost', 'github.com', 'resume-strapi-backend.onrender.com', 'api.djilali.tabbouche.net']
   }
 }
 
